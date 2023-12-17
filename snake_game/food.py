046bb121dd1ec79cb3ea005c92ee1c570df94b94
akshaykamble139 <kakshayiitk@gmail.com>
2023-12-17 18:57:10 +0530

    Snake game added

---
+++ snake_game/food.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
+        self.color("green")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+
+        while random_x == self.xcor() and random_y == self.ycor():
+            random_x = random.randint(-280, 280)
+            random_y = random.randint(-280, 280)
+
+        self.goto(random_x, random_y)
+
---
+++ snake_game/scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+ALIGN = "center"
+FONT = ('Arial', 20, 'normal')
+GAME_OVER = "GAME OVER"
+
+class ScoreBoard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+
+        self.score = 0
+        self.penup()
+        self.hideturtle()
+        self.goto(0,270)
+        self.color("white")
+        self.write(f"Score: {self.score}", False, ALIGN, FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score: {self.score}", False, ALIGN, FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(GAME_OVER, False, ALIGN, FONT)
